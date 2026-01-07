bc2a0f3ad7fce2e74c7e3b129f6ce9cf63da57d2
Felipe Castro <castroofelipee19@gmail.com>
2026-01-07 17:14:42 -0300

    setup schema

---
+++ schemas/users.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    username: str
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    flags: int
+
+    class Config:
+        from_attributes = True
